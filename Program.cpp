1459127233b6f4c98ef38d82501dc5dcb5ab1f5a
lgelbman <lgelbman@student.touro.edu>
2021-03-10 03:14:34 -0500

    finished quicksort and added timer

--- Program.cpp
+++ Program.cpp
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>
 
-using namespace std;
+#include <chrono>
+using std::chrono::system_clock;
+using std::chrono::steady_clock;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::milliseconds;
 
+//using namespace std;
+
+void sort(int array[], int size);
 bool isSorted(int array[], int size);
 void bubbleSort(int array[], int size);
 void swap(int* num1, int* num2);
@@ -10,17 +19,29 @@ double unsortedMetric(int array[], int size);
 int* partition(int array[], int size);
 void quickSort(int array[], int size);
 int sizeOfSubArray(int* start, int* end);
+void fillArrayWithRandomNumbers(int* array);
+void runMySort();
+void runStandardSort();
+template <typename Func>
+long long TimeFunc(Func f);
 
 const int CUT_OFF = 100000000;
+const int arraySize = 1000000;
+int* array1;
+int* array2;
 
 
 int main() {
-    int array[] = {2,5,1,4,7,4,6,21,100,7,45,2,3,45,43,49,65,123,2,34,54};
-    quickSort(array, 21);
-    for (int i = 0; i < 21; i++) {
-        cout << array[i] << endl;
-    }
-    return 0;
+    array1 = new int[arraySize];
+    array2 = new int[arraySize];
+    fillArrayWithRandomNumbers(array1);
+    fillArrayWithRandomNumbers(array2);
+    long long time1 = TimeFunc(runStandardSort);
+    long long time2 = TimeFunc(runMySort);
+
+    std::cout << "time using library: " << time1 << std::endl;
+    std::cout << "time using our sort: " << time2 << std::endl;
+
 }
 
 void sort(int array[], int size) {
@@ -69,10 +90,10 @@ void swap(int* num1, int* num2) {
 
 //modified from pseudocode on https://en.wikipedia.org/wiki/Quicksort
 void quickSort(int array[], int size) {
-    if (size > 1) {
+    if (size >= 1) {
         int* pivot = partition(array, size);
-        quickSort(array, sizeOfSubArray(array, pivot));
-        quickSort(pivot + 1, (size - sizeOfSubArray(array, pivot)));
+        quickSort(array, (sizeOfSubArray(array, pivot) ));
+        quickSort(pivot + 1, (size - sizeOfSubArray(array, pivot) - 1));
     }
 }
 
@@ -85,12 +106,14 @@ int* partition(int array[], int size) {
             ptr1++;
         }
     }
-    swap(pivot, ptr1);
+    //swap(pivot, ptr1);
+    swap(ptr1, &array[size - 1]);
     return ptr1;
 }
 
 int sizeOfSubArray(int* start, int* end) {
-    return (int) (end - start);
+    int size = (int) (end - start);
+    return size;
 }
 
 
@@ -106,5 +129,29 @@ double unsortedMetric(int array[], int size) {
     }
     double metric = ( (double) acc) / ( (double) size);
     return metric;
+}
+
+void fillArrayWithRandomNumbers(int* array) {
+    for (int i = 0; i < arraySize; i++) {
+        array[i] = rand() % 100;
+    }
+}
+
+
+void runMySort() {
+    sort(array1, arraySize);
+}
+
+void runStandardSort() {
+    std::sort(array2, array2 + arraySize);
+}
+
+//copied from canvas
+template <typename Func>
+long long TimeFunc(Func f) {
+    auto begin = steady_clock::now();
+    f();
+    auto end = steady_clock::now();
+    return duration_cast<milliseconds>(end - begin).count();
 }
 
